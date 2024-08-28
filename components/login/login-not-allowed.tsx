0bb13369ea14c5a39344c7286c0ff12939c6af3b
dberos <dberos.dev@gmail.com>
2024-08-28 14:09:16 +0300

    fix not allowed login after updates

--- components/login/login-not-allowed.tsx
+++ components/login/login-not-allowed.tsx
@@ -1,6 +1,12 @@
+"use client";
+
+import useHandleLogin from "@/hooks/use-handle-login";
 import { Skeleton } from "../ui/skeleton";
 
 const LoginNotAllowed = () => {
+
+    useHandleLogin();
+
     return ( 
         <div className="-mt-20 flex flex-col justify-center space-y-8 p-2">
             <Skeleton className="h-10 w-full" />
