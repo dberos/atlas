dd94909eef6eb08a33bf648c3de1fd786bda687b
dberos <dberos.dev@gmail.com>
2024-10-07 16:54:29 +0300

    quick fix

--- components/navbar/handle-protect-route.tsx
+++ components/navbar/handle-protect-route.tsx
@@ -1,6 +1,7 @@
 import { HandleProtectRouteType } from "@/types";
 
 // For desktop, protect a specific route and redirect after if not logged in
+// And useHandleLogin is called at the login form
 const handleProtectRoute = ({
     redirectUrl,
     user,
