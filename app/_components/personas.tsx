69d2cbe2d9fb4c947669fba50e9a0124617934a6
dberos <dberos.dev@gmail.com>
2024-10-07 14:03:16 +0300

    home personas fixes

--- app/_components/personas.tsx
+++ app/_components/personas.tsx
@@ -1,5 +1,6 @@
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 const Personas = () => {
     return ( 
@@ -10,9 +11,12 @@ const Personas = () => {
                     from-slate-300 via-orange-200 to-slate-300 dark:from-slate-800 dark:via-orange-500 dark:to-slate-800 
                     lg:from-orange-300 lg:via-orange-200 lg:to-slate-300 lg:dark:from-orange-500 lg:dark:via-orange-300 lg:dark:to-slate-800" />
                     <div className="flex items-center justify-center w-full h-10">
-                        <h3 className="mt-4 text-lg 2xl:text-xl font-medium text-center cursor-pointer">
+                        <Link
+                        href='/profile/add-internship'
+                        className="mt-4 text-lg 2xl:text-xl font-medium text-center cursor-pointer"
+                        >
                             Προσθήκη Πρακτικής Άσκησης
-                        </h3>
+                        </Link>
                     </div>
                     <div className="flex items-center justify-center w-full h-20">
                         <p className="mt-5 max-w-sm text-center text-sm lg:text-base 2xl:text-md">
@@ -20,13 +24,18 @@ const Personas = () => {
                         </p>
                     </div>
                     <div className="w-full h-48 lg:h-44 xl:h-48 flex items-center justify-center">
-                        <Image 
-                        src="/home-personas-left.svg"
-                        alt="person uploading a file"
-                        width={100}
-                        height={100}
-                        className="size-4/6 object-contain xl:cursor-pointer xl:hover:scale-105 xl:transition xl:ease-in-out"
-                        />
+                        <Link
+                        href='/profile/add-internship'
+                        className="contents"
+                        >
+                            <Image 
+                            src="/home-personas-left.svg"
+                            alt="person uploading a file"
+                            width={100}
+                            height={100}
+                            className="size-4/6 object-contain xl:cursor-pointer xl:hover:scale-105 xl:transition xl:ease-in-out"
+                            />
+                        </Link>
                     </div>
                 </div>
                 <div className="w-full h-96 lg:w-1/2 flex flex-col items-center justify-center">
@@ -34,9 +43,12 @@ const Personas = () => {
                     from-slate-300 via-orange-200 to-slate-300 dark:from-slate-800 dark:via-orange-500 dark:to-slate-800
                     lg:from-slate-300 lg:via-orange-200 lg:to-orange-300 lg:dark:from-slate-800 lg:dark:via-orange-300 lg:dark:to-orange-500" />
                     <div className="flex items-center justify-center w-full h-10">
-                        <h3 className="mt-4 text-lg 2xl:text-xl font-medium text-center cursor-pointer">
+                        <Link
+                        href='/profile/view-internships'
+                        className="mt-4 text-lg 2xl:text-xl font-medium text-center cursor-pointer"
+                        >
                             Αξιολόγηση Ενδιαφέροντος
-                        </h3>
+                        </Link>
                     </div>
                     <div className="flex items-center justify-center w-full h-20">
                         <p className="mt-5 max-w-sm text-center text-sm lg:text-base 2xl:text-md">
@@ -44,13 +56,18 @@ const Personas = () => {
                         </p>
                     </div>
                     <div className="w-full h-48 lg:h-44 xl:h-48 flex items-center justify-center">
-                        <Image 
-                        src="/home-personas-right.svg"
-                        alt="person searching"
-                        width={100}
-                        height={100}
-                        className="size-4/6 object-contain xl:cursor-pointer xl:hover:scale-105 xl:transition xl:ease-in-out"
-                        />
+                        <Link
+                        href='/profile/view-internships'
+                        className="contents"
+                        >
+                            <Image 
+                            src="/home-personas-right.svg"
+                            alt="person searching"
+                            width={100}
+                            height={100}
+                            className="size-4/6 object-contain xl:cursor-pointer xl:hover:scale-105 xl:transition xl:ease-in-out"
+                            />
+                        </Link>
                     </div>
                 </div>
             </div>
@@ -63,7 +80,11 @@ const Personas = () => {
                     Διαχειριστείτε όλες τις ενδιάμεσες διαδικασίες του τμήματός σας μέσω του προσωπικού σας λογαριασμού στον ΑΤΛΑΣ.
                 </p>
                 <Button size="lg" className="p-4 mt-10">
-                    Μάθετε περισσότερα
+                    <Link
+                    href='/faq'
+                    >
+                        Μάθετε περισσότερα
+                    </Link>
                 </Button>
             </div>
         </div>
